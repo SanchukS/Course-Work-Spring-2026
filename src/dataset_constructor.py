59b287460640c67c02093ff4217545d772970bb9
Sergey Sanchuk <SanchukS@gmail.com>
2026-05-17 15:14:05 +0300

    Created fuction-constructor for dataset: build_dataset (build_dataset_from_session)

---
+++ src/dataset_constructor.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from src.features_extraction import extract_features
+
+def build_dataset_from_session(session_df: pd.DataFrame, window_size: int = 3) -> tuple[list[dict[str, float]], list[int]]:
+    """
+    Проходится по одной сессии пользователя и генерирует X и y.
+    session_df: DataFrame с колонками ['DAY', 'WEIGHT_GR', 'VOLUME_ML', 'COUNT']
+    """
+    X_list = []
+    y_list = []
+    
+    # Нам нужно минимум 1 покупка для фичей и 1 следующая для таргета
+    for i in range(2, len(session_df)):
+        history_df = session_df.iloc[:i]
+        target_row = session_df.iloc[i]
+        
+        features = extract_features(history_df, window_size)
+        target = target_row['DAY'] - history_df.iloc[-1]['DAY']
+        
+        X_list.append(features)
+        y_list.append(target)
+        
+    return X_list, y_list
+
+
+def build_dataset(outflow_data: pd.DataFrame, window_size: int = 3) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Проходит по всем сессиям, генерирует по каждой из них X и y, объединяет в один датасет.
+    outflow_data: DataFrame с колонками ['TOTAL CATEGORY', 'SESSION_ID', 'DAY', 'WEIGHT_GR', 'VOLUME_ML', 'COUNT']
+    """
+
+    grouped_sessions = outflow_data.groupby(["TOTAL CATEGORY", "SESSION_ID"])
+
+    X, y = [], []
+
+    for name, session_df in grouped_sessions:
+        X_session, y_session = build_dataset_from_session(session_df, window_size)
+        X.extend(X_session)
+        y.extend(y_session)
+
+    X_result = np.array(X)
+    y_result = np.array(y)
+
+    return X_result, y_result
